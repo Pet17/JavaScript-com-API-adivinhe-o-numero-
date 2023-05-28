238232f6aebe098654d757a73a932706ad3dfc57
Peterson <128753590+Pet17@users.noreply.github.com>
2023-05-28 02:45:34 -0300

    JavaScript e API

---
+++ app/validacao.js
@@ -0,0 +1,57 @@
+function verificaSeoChutePossuiUmValorValido(chute) {
+    const numero = +chute;
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
+    }
+
+    if (numeroForMaiorOuMenorQueValorPermitido(numero)) {
+        elementoChute.innerHTML += `<div>Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="botao">Jogar novamente</button>
+        `;
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `;
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-down-long"></i></div>
+        `;
+    }
+}
+
+function chuteForInvalido(numero) {
+    return isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+});
+
+
+
+function chuteForInvalido(numero) {
+    return isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+})
