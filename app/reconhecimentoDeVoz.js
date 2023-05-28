efec14907255e25308d48625648daafccc22214e
Peterson <128753590+Pet17@users.noreply.github.com>
2023-05-28 20:03:30 -0300

    Update reconhecimentoDeVoz.js

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,24 +1,24 @@
-const elementoChute = document.getElementById('chute');
+const elementoChute = document.getElementById('chute')
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
-const recognition = new window.SpeechRecognition();
-recognition.lang = 'pt-BR';
-recognition.start();
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-BR'
+recognition.start()
 
-recognition.addEventListener('result', onSpeak);
+recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-    const chute = e.results[0][0].transcript
+    chute = e.results[0][0].transcript
     exibeChuteNaTela(chute)
     verificaSeOChutePossuiUmValorValido(chute)
 }
 
 function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
-    <div>Você disse:</div>
-    <span class="box">${chute}</span>
-    `;
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
 }
 
 recognition.addEventListener('end', () => recognition.start())
